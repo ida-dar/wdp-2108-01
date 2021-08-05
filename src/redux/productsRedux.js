bd205c5be56eda960f371e5b47b6885fadf2a358
Ida Darmochwał <ida.mdart@gmail.com>
2021-08-05 20:47:45 +0200

    Enable star rating of products

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,9 +5,33 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+/* ACTIONS */
+
+// action name creator
+const reducerName = 'product';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+export const ADD_RATING = createActionName('ADD_RATING');
+
+// action creators
+export const addRating = payload => ({ payload, type: ADD_RATING });
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case ADD_RATING: {
+      return statePart.map(currentRating => {
+        if (currentRating.id !== action.payload.id) {
+          return currentRating;
+        }
+
+        return {
+          ...currentRating,
+          userRating: action.payload.userRating,
+        };
+      });
+    }
     default:
       return statePart;
   }
