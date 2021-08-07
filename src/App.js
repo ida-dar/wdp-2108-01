80cfab1eeecd7dae64772591a80dc332298ee68b
Konrad Jówko <konrad.jowko@gmail.com>
2021-08-07 17:24:23 +0200

    Add basic Shop component structure

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './styles/global.scss';
 import MainLayout from './components/layout/MainLayout/MainLayoutContainer';
 import Homepage from './components/views/Homepage/Homepage';
 import ProductList from './components/views/ProductList/ProductList';
+import Shop from './components/views/Shop/Shop';
 import ProductPage from './components/views/ProductPage/ProductPage';
 
 const App = () => (
@@ -17,7 +18,7 @@ const App = () => (
       <MainLayout>
         <Switch>
           <Route exact path={'/'} component={Homepage} />
-          <Route exact path={'/shop/:categoryId'} component={ProductList} />
+          <Route exact path={'/shop/:categoryId'} component={Shop} />
           <Route exact path={'/product/:productId'} component={ProductPage} />
         </Switch>
       </MainLayout>
