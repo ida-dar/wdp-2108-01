c48d9c19277184b90275d30ba84a3122499c9430
Konrad Jówko <konrad.jowko@gmail.com>
2021-08-08 13:30:36 +0200

    Add basic Search component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MainLayout from './components/layout/MainLayout/MainLayoutContainer';
 import Homepage from './components/views/Homepage/Homepage';
 import ProductList from './components/views/ProductList/ProductList';
 import ProductPage from './components/views/ProductPage/ProductPage';
+import Search from './components/views/Search/Search';
 
 const App = () => (
   <Provider store={store}>
@@ -19,6 +20,7 @@ const App = () => (
           <Route exact path={'/'} component={Homepage} />
           <Route exact path={'/shop/:categoryId'} component={ProductList} />
           <Route exact path={'/product/:productId'} component={ProductPage} />
+          <Route exact path={'/search'} component={Search} />
         </Switch>
       </MainLayout>
     </BrowserRouter>
