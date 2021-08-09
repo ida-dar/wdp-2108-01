2f2f8604600541d99a8a17023048192a398d320f
asiulijka <j.m.h.zielinska@gmail.com>
2021-08-10 01:22:10 +1000

    Added OnSpecial component and its styles

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -8,11 +8,13 @@ import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import Promotion from '../../features/Promotion/PromotionContainer';
 import Gallery from '../../features/Gallery/GalleryContainer';
 import Brands from '../../layout/Brands/BrandsContainer';
+import OnSpecial from '../../features/OnSpecial/OnSpecialContainer';
 
 const Homepage = () => (
   <div className={styles.root}>
     <Promotion />
     <FeatureBoxes />
+    <OnSpecial />
     <NewFurniture />
     <Gallery />
     <Brands />
