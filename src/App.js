750200b93253577cdd7884bba46dd4861638a759
Konrad Jówko <konrad.jowko@gmail.com>
2021-08-10 18:52:19 +0200

    Changes shop component to ProductList component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import './styles/global.scss';
 import MainLayout from './components/layout/MainLayout/MainLayoutContainer';
 import Homepage from './components/views/Homepage/Homepage';
 import ProductList from './components/views/ProductList/ProductList';
-import Shop from './components/views/Shop/Shop';
 import ProductPage from './components/views/ProductPage/ProductPage';
 
 const App = () => (
@@ -18,7 +17,7 @@ const App = () => (
       <MainLayout>
         <Switch>
           <Route exact path={'/'} component={Homepage} />
-          <Route exact path={'/shop/:categoryId'} component={Shop} />
+          <Route exact path={'/shop/:categoryId'} component={ProductList} />
           <Route exact path={'/product/:productId'} component={ProductPage} />
         </Switch>
       </MainLayout>
--- src/components/views/Shop/Shop.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Shop from './Shop';
-
-describe('ProductList', () => {
-  it('renders without crashing', () => {
-    shallow(<Shop />);
-  });
-});
