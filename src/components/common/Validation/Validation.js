b13ccb2d460024000f874eb777a3a7a8fd822006
Konrad Jówko <konrad.jowko@gmail.com>
2021-08-10 20:32:27 +0200

    Add Login funcionality

---
+++ src/components/common/Validation/Validation.js
@@ -0,0 +1,24 @@
+export function validation(event) {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const container = document.querySelector('.cont');
+  const fail = document.getElementById('fail');
+
+  if (fail) fail.remove();
+
+  if (email === 'admin') {
+    if (password === 'pass') {
+      document.location = `${process.env.PUBLIC_URL}/`;
+    } else {
+      if (fail) {
+        fail.remove();
+      }
+      container.insertAdjacentHTML(
+        'afterbegin',
+        "<span id='fail'> Wrong Password </span>"
+      );
+    }
+  } else {
+    container.insertAdjacentHTML('afterbegin', "<span id='fail'> Wrong Email </span>");
+  }
+}
