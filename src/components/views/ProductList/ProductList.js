b066ab98c26374b7f4deb016000ee4e3b70a8c2c
Konrad Jówko <konrad.jowko@gmail.com>
2021-08-16 20:50:46 +0200

    Add Brands section to shop subpage

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 import Button from '../../common/Button/Button';
+import Brands from '../../layout/Brands/BrandsContainer';
 
 const ProductList = () => (
   <div className={styles.root}>
@@ -30,7 +31,7 @@ const ProductList = () => (
       <div className={styles.productList + ' col-9'}> Product List </div>
       <div className={styles.filters + ' col-3'}> Filters </div>
     </div>
-    <div className={styles.brands}> Brands </div>
+    <Brands />
   </div>
 );
 
