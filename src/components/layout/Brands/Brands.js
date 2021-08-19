e21557ba067b79987c9ea7243c9a8f7d5d3a4bc7
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-08-19 20:25:23 +0200

    RWD for Brands

--- src/components/layout/Brands/Brands.js
+++ src/components/layout/Brands/Brands.js
@@ -58,62 +58,48 @@ class Brands extends React.Component {
       columnClass = 'col-6';
       productCount = 2;
     }
-    /*
-const Brands = ({ brands }) => {
-  const [brand] = useState(6);
-  const [activePage, setActivePage] = useState(0);
 
-  const handleClickPrev = e => {
-    e.preventDefault();
-    setActivePage(activePage === 0 ? brands.length / brand - 1 : activePage - 1);
-  };
-
-  const handleClickNext = e => {
-    e.preventDefault();
-    setActivePage(activePage === brands.length / brand - 1 ? 0 : activePage + 1);
-  };
-*/
     return (
-      //<Swipeable leftAction={handleClickPrev} rightAction={handleClickNext}>
-      <div className={styles.root}>
-        <div className='container'>
-          <div className={'col-auto ' + styles.heading}>
-            <h3>Brands</h3>
-          </div>
-          <div className={'row ' + styles.brands}>
-            <div className='col-1'>
-              <Button
-                className={styles.button}
-                variant='small'
-                onClick={() => this.handleClickPrev(activePage - 1)}
-              >
-                <FontAwesomeIcon icon={faChevronLeft} />
-              </Button>
+      <Swipeable leftAction={this.handleClickPrev} rightAction={this.handleClickNext}>
+        <div className={styles.root}>
+          <div className='container'>
+            <div className={'col-auto ' + styles.heading}>
+              <h3>Brands</h3>
             </div>
-            <div className='col-10'>
-              <div className='row'>
-                {brands
-                  .slice(activePage * productCount, (activePage + 1) * productCount)
-                  .map(item => (
-                    <div key={item.id} className={columnClass}>
-                      <Brand {...item} />
-                    </div>
-                  ))}
+            <div className={'row ' + styles.brands}>
+              <div className='col-1'>
+                <Button
+                  className={styles.button}
+                  variant='small'
+                  onClick={() => this.handleClickPrev(activePage - 1)}
+                >
+                  <FontAwesomeIcon icon={faChevronLeft} />
+                </Button>
               </div>
-            </div>
-            <div className='col-1'>
-              <Button
-                className={styles.button}
-                variant='small'
-                onClick={() => this.handleClickNext(activePage + 1, pagesCount)}
-              >
-                <FontAwesomeIcon icon={faChevronRight} />
-              </Button>
+              <div className='col-10'>
+                <div className='row'>
+                  {brands
+                    .slice(activePage * productCount, (activePage + 1) * productCount)
+                    .map(item => (
+                      <div key={item.id} className={columnClass}>
+                        <Brand {...item} />
+                      </div>
+                    ))}
+                </div>
+              </div>
+              <div className='col-1'>
+                <Button
+                  className={styles.button}
+                  variant='small'
+                  onClick={() => this.handleClickNext(activePage + 1, pagesCount)}
+                >
+                  <FontAwesomeIcon icon={faChevronRight} />
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      //</Swipeable>
+      </Swipeable>
     );
   }
 }
