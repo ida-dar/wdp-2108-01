cf21d50757a88d4f25470f0590ed67b29d7d4956
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-08-19 19:32:08 +0200

    Add RWD

--- src/components/layout/Brands/Brands.js
+++ src/components/layout/Brands/Brands.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Brands.module.scss';
 
@@ -8,24 +8,73 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 import Button from '../../common/Button/Button';
 import Swipeable from '../../features/Swipeable/Swipeable';
 
+class Brands extends React.Component {
+  state = {
+    activePage: 0,
+    currRWD: 'desktops',
+  };
+
+  componentDidMount() {
+    const func = () => this.setDisplay(this.props.rwd, this.state.currRWD);
+    func();
+    window.addEventListener('resize', func);
+  }
+
+  setDisplay(display, currDisplay) {
+    if (currDisplay !== display) {
+      this.setState({ currRWD: display });
+    }
+  }
+
+  handleClickPrev(newPage) {
+    if (-1 < newPage) {
+      this.setState({ activePage: newPage });
+    } else {
+      return false;
+    }
+  }
+  handleClickNext(newPage, pagesCount) {
+    if (newPage < pagesCount) {
+      this.setState({ activePage: newPage });
+    } else {
+      return false;
+    }
+  }
+
+  render() {
+    const { brands } = this.props;
+    const { activePage, currRWD } = this.state;
+    const pagesCount = Math.ceil(brands.length / 6);
+
+    let columnClass = 'col-2';
+    let productCount = 6;
+    if (currRWD === 'desktops') {
+      columnClass = 'col-2';
+      productCount = 6;
+    } else if (currRWD === 'tablets') {
+      columnClass = 'col-3';
+      productCount = 4;
+    } else if (currRWD === 'phones') {
+      columnClass = 'col-6';
+      productCount = 2;
+    }
+    /*
 const Brands = ({ brands }) => {
   const [brand] = useState(6);
   const [activePage, setActivePage] = useState(0);
 
   const handleClickPrev = e => {
     e.preventDefault();
-
     setActivePage(activePage === 0 ? brands.length / brand - 1 : activePage - 1);
   };
 
   const handleClickNext = e => {
     e.preventDefault();
-
     setActivePage(activePage === brands.length / brand - 1 ? 0 : activePage + 1);
   };
-
-  return (
-    <Swipeable leftAction={handleClickPrev} rightAction={handleClickNext}>
+*/
+    return (
+      //<Swipeable leftAction={handleClickPrev} rightAction={handleClickNext}>
       <div className={styles.root}>
         <div className='container'>
           <div className={'col-auto ' + styles.heading}>
@@ -36,7 +85,7 @@ const Brands = ({ brands }) => {
               <Button
                 className={styles.button}
                 variant='small'
-                onClick={handleClickPrev}
+                onClick={() => this.handleClickPrev(activePage - 1)}
               >
                 <FontAwesomeIcon icon={faChevronLeft} />
               </Button>
@@ -44,9 +93,9 @@ const Brands = ({ brands }) => {
             <div className='col-10'>
               <div className='row'>
                 {brands
-                  .slice(activePage * brand, (activePage + 1) * brand)
+                  .slice(activePage * productCount, (activePage + 1) * productCount)
                   .map(item => (
-                    <div key={item.id} className='col-2'>
+                    <div key={item.id} className={columnClass}>
                       <Brand {...item} />
                     </div>
                   ))}
@@ -56,7 +105,7 @@ const Brands = ({ brands }) => {
               <Button
                 className={styles.button}
                 variant='small'
-                onClick={handleClickNext}
+                onClick={() => this.handleClickNext(activePage + 1, pagesCount)}
               >
                 <FontAwesomeIcon icon={faChevronRight} />
               </Button>
@@ -64,11 +113,13 @@ const Brands = ({ brands }) => {
           </div>
         </div>
       </div>
-    </Swipeable>
-  );
-};
+      //</Swipeable>
+    );
+  }
+}
 
 Brands.propTypes = {
+  rwd: PropTypes.string,
   brands: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string,
